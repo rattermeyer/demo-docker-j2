ac1dd0d3d76790c1554cbb4f3dd8716ee189300f
Richard Attermeyer <richard.attermeyer@gmail.com>
2016-02-08 21:27:21 +0100

    initial commit

---
+++ scripts/provision.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#if [ ! -d ansible-dev-desktop ]; then
+#  git clone https://github.com/rattermeyer/ansible-dev-desktop.git
+#  git checkout ubuntu-unity
+#  cd ansible-dev-desktop
+#else
+#  cd ansible-dev-desktop
+#  git pull
+#fi
+#cp /vagrant/external_vars.yml /home/vagrant/ansible-dev-desktop
+#ansible-playbook -i inventory dev.yml
